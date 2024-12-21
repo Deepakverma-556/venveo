71bf4dfe4dd3765ae816d0120faa00c3bc4b6ca8
Deepak Verma <deepakradialcode@gmail.com>
2024-12-21 11:24:57 +0530

    create heading and description

--- src/common/Description.jsx
+++ src/common/Description.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Description = () => {
+const Description = ({myClass, text}) => {
   return (
-    <div>Description</div>
+      <p className={`${myClass} font-maisonBook text-[15px] leading-5 text-black `}>{text}</p>
   )
 }
 
--- src/common/Heading.jsx
+++ src/common/Heading.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Heading = () => {
-  return (
-    <div>Heading</div>
-  )
+const Heading = ({ text, myClass, spanText, secondText, image, myAlt, imgClass }) => {
+    return (
+        <h1 className={`${myClass} font-sohne text-black text-[80px] leading-[70px] text-center`}>{text} <span className={`relative`}>{spanText} <img src={image} alt={myAlt} className={`absolute ${imgClass}`} /></span>{secondText}</h1>
+    )
 }
 
 export default Heading
